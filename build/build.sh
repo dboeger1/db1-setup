167adab4504cfbae617c5036db629d81fee9f155
David Michael Boeger <dmboeger@gmail.com>
2023-07-25 01:41:41 +0000

    Improve help messages.

--- build/build.sh
+++ build/build.sh
@@ -5,8 +5,6 @@
 # Basic program information.
 #
 PROGRAM="$(basename ${0})"
-VERSION="0.0.1"
-INFO="${PROGRAM} v${VERSION}"
 PWD="$(pwd)"
 
 
@@ -16,63 +14,91 @@ PWD="$(pwd)"
 OPTSTR=""
 
 CHAR_OPT_HELP="h"
-DEFAULT_OPT_HELP="false"
-opt_help="${DEFAULT_OPT_HELP}"
+opt_help="false"
 OPTSTR+="${CHAR_OPT_HELP}"
 
 CHAR_OPT_USAGE="u"
-DEFAULT_OPT_USAGE="false"
-opt_usage="${DEFAULT_OPT_USAGE}"
+opt_usage="false"
 OPTSTR+="${CHAR_OPT_USAGE}"
 
 CHAR_OPT_BUILD="b"
-DEFAULT_OPT_BUILD=""
-opt_build="${DEFAULT_OPT_BUILD}"
+opt_build="false"
+STR_ARG_BUILD="ARTIFACT"
+STR_ARG_BUILD_SRC="src"
+STR_ARG_BUILD_DEB="deb"
+STR_ARG_BUILD_RPM="rpm"
+STR_ARG_BUILD_ALL="all"
+DEFAULT_ARG_BUILD=""
+arg_build="${DEFAULT_ARG_BUILD}"
 OPTSTR+="${CHAR_OPT_BUILD}:"
 
 CHAR_OPT_CLEAN="c"
-DEFAULT_OPT_CLEAN=""
-opt_clean="${DEFAULT_OPT_CLEAN}"
+opt_clean="false"
+STR_ARG_CLEAN="ARTIFACT"
+STR_ARG_CLEAN_SRC="src"
+STR_ARG_CLEAN_DEB="deb"
+STR_ARG_CLEAN_RPM="rpm"
+STR_ARG_CLEAN_ALL="all"
+DEFAULT_ARG_CLEAN=""
+arg_clean="${DEFAULT_ARG_CLEAN}"
 OPTSTR+="${CHAR_OPT_CLEAN}:"
 
 CHAR_OPT_FORCE="f"
-DEFAULT_OPT_FORCE="false"
-opt_force="${DEFAULT_OPT_FORCE}"
+opt_force="false"
 OPTSTR+="${CHAR_OPT_FORCE}"
 
-CHAR_OPT_INFO="i"
-DEFAULT_OPT_INFO="false"
-opt_info="${DEFAULT_OPT_INFO}"
-OPTSTR+="${CHAR_OPT_INFO}"
-
 CHAR_OPT_VERBOSE="v"
-DEFAULT_OPT_VERBOSE="false"
-opt_verbose="${DEFAULT_OPT_VERBOSE}"
+opt_verbose="false"
 OPTSTR+="${CHAR_OPT_VERBOSE}"
 
 CHAR_OPT_SRCDIR="S"
-DEFAULT_OPT_SRCDIR="${PWD}"
-opt_srcdir="${DEFAULT_OPT_SRCDIR}"
+opt_srcdir="false"
+STR_ARG_SRCDIR="SRC_DIR"
+DEFAULT_ARG_SRCDIR="${PWD}"
+arg_srcdir="${DEFAULT_ARG_SRCDIR}"
 OPTSTR+="${CHAR_OPT_SRCDIR}:"
 
 CHAR_OPT_DESTDIR="D"
-DEFAULT_OPT_DESTDIR="${PWD}"
-opt_destdir="${DEFAULT_OPT_DESTDIR}"
+opt_destdir="false"
+STR_ARG_DESTDIR="DEST_DIR"
+DEFAULT_ARG_DESTDIR="${PWD}"
+arg_destdir="${DEFAULT_ARG_DESTDIR}"
 OPTSTR+="${CHAR_OPT_DESTDIR}:"
 
 CHAR_OPT_VERSION="V"
-DEFAULT_OPT_VERSION="0.0.1"
-opt_version="${DEFAULT_OPT_VERSION}"
+opt_version="false"
+STR_ARG_VERSION="VERSION"
+DEFAULT_ARG_VERSION="0.0.1"
+arg_version="${DEFAULT_ARG_VERSION}"
 OPTSTR+="${CHAR_OPT_VERSION}:"
 
 
 #
 # Usage statement.
 #
-USAGE=$(cat << END
-Usage: ${PROGRAM} [-hu] [-b ARTIFACT | -c ARTIFACT] [-fiv]
-           [-S SRC_DIR] [-D DEST_DIR] [-V VERSION]
+USAGE="$(cat << END
+Usage: %s {-%c | -%c | -%c %s | -%c %s} [-%c%c]
+        [-%c %s] [-%c %s] [-%c %s]
+    Run \"%s -%c\" for more help.
 END
+)"
+USAGE=$(printf "${USAGE}"   \
+    "${PROGRAM}"            \
+    "${CHAR_OPT_HELP}"      \
+    "${CHAR_OPT_USAGE}"     \
+    "${CHAR_OPT_BUILD}"     \
+    "${STR_ARG_BUILD}"      \
+    "${CHAR_OPT_CLEAN}"     \
+    "${STR_ARG_CLEAN}"      \
+    "${CHAR_OPT_FORCE}"     \
+    "${CHAR_OPT_VERBOSE}"   \
+    "${CHAR_OPT_SRCDIR}"    \
+    "${STR_ARG_SRCDIR}"     \
+    "${CHAR_OPT_DESTDIR}"   \
+    "${STR_ARG_DESTDIR}"    \
+    "${CHAR_OPT_VERSION}"   \
+    "${PROGRAM}"            \
+    "${CHAR_OPT_HELP}"      \
 )
 
 exit_usage()
@@ -91,44 +117,40 @@ exit_usage_err()
 #
 # Help message.
 #
-HELP=$(cat << END
+HELP="$(cat << END
 ${USAGE}
 
 Help options:
 
-    -h              Display this help message, which includes the usage
-                    statement displayed by -u. This option overrides all other
-                    options except for -i. When none of -h, -u, -b, and -c are
-                    specified, this is the default behavior.
+    -%c              Display this help message. This option overrides all other
+                    options.
 
-    -u              Display a brief usage statement. This option overrides all
-                    other options except for -h and -i. This option is
-                    overridden by -h, which includes the usage statement in its
-                    help message.
+    -%c              Display a brief usage statement. This option overrides all
+                    other options except for -%c.
 
 Mode options:
 
-    -b ARTIFACT     Build the specified artifacts. Artifacts are built in the
+    -%c %s     Build the specified artifacts. Artifacts are built in the
                     destination directory from sources in the source directory.
                     By default, the current working directory is treated as both
                     the source and destination directory, but alternative values
-                    may be specified with -S and -D, respectively. The ARTIFACT
-                    argument specifies which artifacts to build. Valid values
-                    are "src", "rpm", "deb", and "all". "src" produces a bundle
-                    of the sources. "rpm" and "deb" produce a package of the
-                    respective format from the source bundle. As such, if the
-                    source bundle is not found, both "rpm" and "deb" first
-                    perform the "src" build. By extension, "all" produces all of
+                    may be specified with -%c and -%c, respectively. The
+                    %s argument specifies which artifacts to build. Valid
+                    values are "%s", "%s", "%s", and "%s". "%s" produces a
+                    bundle of the sources. "%s" and "%s" produce a package of
+                    the respective format from the source bundle. As such, if
+                    the source bundle is not found, both "%s" and "%s" first
+                    perform the "%s" build. By extension, "%s" produces all of
                     the previously mentioned artifacts. If multiple instances of
                     this option are provided, all specified artifacts are built.
 
-    -c ARTIFACT     Clean the specified build artifacts from the destination
+    -%c %s     Clean the specified build artifacts from the destination
                     directory. By default, the current working directory is
                     treated as the destination directory, but an alternative
-                    value may be specified with -D. The ARTIFACT argument
-                    specifies which artifacts to clean. Valid values are "src",
-                    "rpm", "deb", and "all". "src" removes the source bundle.
-                    "rpm" and "deb" remove the respective package. "all" removes
+                    value may be specified with -%c. The %s argument
+                    specifies which artifacts to clean. Valid values are "%s",
+                    "%s", "%s", and "%s". "%s" removes the source bundle.
+                    "%s" and "%s" remove the respective package. "%s" removes
                     all of the previously mentioned artifacts. If multiple
                     instances of this option are provided, all specified
                     artifacts are removed. If none of the build artifacts remain
@@ -137,41 +159,76 @@ Mode options:
 
 Common mode modifier options:
 
-    -S SRC_DIR      Specify an alternative source directory containing sources
+    -%c %s      Specify an alternative source directory containing sources
                     to be built. By default, the current working directory is
                     treated as the source directory.
 
-    -D DEST_DIR     Specify an alternative destination directory for build
+    -%c %s     Specify an alternative destination directory for build
                     artifacts. By default, the current working directory is
                     treated as the destination directory.
 
-    -V VERSION      Specify the version string of the sources being built. By
-                    default, "${DEFAULT_OPT_VERSION}" is used.
+    -%c %s      Specify the version string of the sources being built. By
+                    default, "%s" is used.
 
 Build mode modifier options:
 
-    -f              Force rebuilding and overwriting of existing artifacts in
-                    the destination directory. Without this option, -b does not
+    -%c              Force rebuilding and overwriting of existing artifacts in
+                    the destination directory. Without this option, -%c does not
                     overwrite existing artifact files.
 
 Informational options:
 
-    -i              Display program information, including version.
-
-    -v              Enable verbose output.
+    -%c              Enable verbose output.
 END
+)"
+HELP=$(printf "${HELP}"         \
+    "${CHAR_OPT_HELP}"          \
+    "${CHAR_OPT_USAGE}"         \
+    "${CHAR_OPT_HELP}"          \
+    "${CHAR_OPT_BUILD}"         \
+    "${STR_ARG_BUILD}"          \
+    "${CHAR_OPT_SRCDIR}"        \
+    "${CHAR_OPT_DESTDIR}"       \
+    "${STR_ARG_BUILD}"          \
+    "${STR_ARG_BUILD_SRC}"      \
+    "${STR_ARG_BUILD_DEB}"      \
+    "${STR_ARG_BUILD_RPM}"      \
+    "${STR_ARG_BUILD_ALL}"      \
+    "${STR_ARG_BUILD_SRC}"      \
+    "${STR_ARG_BUILD_DEB}"      \
+    "${STR_ARG_BUILD_RPM}"      \
+    "${STR_ARG_BUILD_DEB}"      \
+    "${STR_ARG_BUILD_RPM}"      \
+    "${STR_ARG_BUILD_SRC}"      \
+    "${STR_ARG_BUILD_ALL}"      \
+    "${CHAR_OPT_CLEAN}"         \
+    "${STR_ARG_CLEAN}"          \
+    "${CHAR_OPT_DESTDIR}"       \
+    "${STR_ARG_CLEAN}"          \
+    "${STR_ARG_CLEAN_SRC}"      \
+    "${STR_ARG_CLEAN_DEB}"      \
+    "${STR_ARG_CLEAN_RPM}"      \
+    "${STR_ARG_CLEAN_ALL}"      \
+    "${STR_ARG_CLEAN_SRC}"      \
+    "${STR_ARG_CLEAN_DEB}"      \
+    "${STR_ARG_CLEAN_RPM}"      \
+    "${STR_ARG_CLEAN_ALL}"      \
+    "${CHAR_OPT_SRCDIR}"        \
+    "${STR_ARG_SRCDIR}"         \
+    "${CHAR_OPT_DESTDIR}"       \
+    "${STR_ARG_DESTDIR}"        \
+    "${CHAR_OPT_VERSION}"       \
+    "${STR_ARG_VERSION}"        \
+    "${DEFAULT_ARG_VERSION}"    \
+    "${CHAR_OPT_FORCE}"         \
+    "${CHAR_OPT_BUILD}"         \
+    "${CHAR_OPT_VERBOSE}"       \
 )
 
 exit_help()
 {
     echo "${HELP}"
     exit 0
-}
-
-exit_help_err()
-{
-    echo "${HELP}" 1>&2
-    exit 1
 }
 
 
@@ -188,25 +245,30 @@ do
         opt_usage="true"
         ;;
     "${CHAR_OPT_BUILD}")
-        opt_build="${OPTARG}"
+        opt_build="true"
+        arg_build="${OPTARG}"
         ;;
     "${CHAR_OPT_CLEAN}")
-        opt_clean="${OPTARG}"
+        opt_clean="true"
+        arg_clean="${OPTARG}"
         ;;
     "${CHAR_OPT_FORCE}")
         opt_force="true"
-        ;;
-    "${CHAR_OPT_INFO}")
-        opt_info="true"
         ;;
     "${CHAR_OPT_VERBOSE}")
         opt_verbose="true"
         ;;
     "${CHAR_OPT_SRCDIR}")
-        opt_srcdir="${OPTARG}"
+        opt_srcdir="true"
+        arg_srcdir="${OPTARG}"
         ;;
     "${CHAR_OPT_DESTDIR}")
-        opt_destdir="${OPTARG}"
+        opt_destdir="true"
+        arg_destdir="${OPTARG}"
+        ;;
+    "${CHAR_OPT_VERSION}")
+        opt_version="true"
+        arg_version="${OPTARG}"
         ;;
     "?")
         exit_usage_err
@@ -216,20 +278,22 @@ done
 
 
 #
-# Perform preemptive tasks prior to validation of remaining options.
+# Validate and perform preemptive tasks.
 #
-if [ "${opt_info}" = "true" ]
-then
-    echo "${INFO}"
-fi
-
-if \
-    [ "${opt_help}" != "true" ] && \
-    [ "${opt_usage}" != "true" ] && \
-    [ "${opt_build}" != "true" ] && \
+if
+    [ "${opt_help}"  != "true" ] &&
+    [ "${opt_usage}" != "true" ] &&
+    [ "${opt_build}" != "true" ] &&
     [ "${opt_clean}" != "true" ]
 then
+    printf "Must specify at least one: {-%c, -%c, -%c, -%c}\n" \
+        "${CHAR_OPT_HELP}"  \
+        "${CHAR_OPT_USAGE}" \
+        "${CHAR_OPT_BUILD}" \
+        "${CHAR_OPT_CLEAN}" 1>&2
+    exit_usage_err
 fi
+
 if [ "${opt_help}" = "true" ]
 then
     exit_help
@@ -244,106 +308,67 @@ fi
 #
 # Validate remaining options.
 #
-if [ "${opt_clean}" != "true" ] && [ "${opt_clean}" != "false" ]
+if [ "${opt_build}" = "true" ] && [ "${opt_clean}" = "true" ]
 then
-    echo "Invalid clean option: \"${opt_clean}\"" 1>&2
-    usage_err
+    echo "-${CHAR_OPT_BUILD} and -${CHAR_OPT_CLEAN} are mutually exclusive." \
+        1>&2
+    exit_usage_err
 fi
 
-# Force
-if [ "${opt_force}" != "true" ] && [ "${opt_force}" != "false" ]
+if
+    [ "${opt_build}" = "true" ]     &&
+    [ "${arg_build}" != "src" ] &&
+    [ "${arg_build}" != "rpm" ] &&
+    [ "${arg_build}" != "deb" ] &&
+    [ "${arg_build}" != "all" ]
 then
-    echo "Invalid force option: \"${opt_force}\"" 1>&2
-    usage_err
+    echo "Invalid -${CHAR_OPT_BUILD} argument: \"${arg_build}\"" 1>&2
+    echo "Must be one of: {\"src\", \"rpm\", \"deb\", \"all\"}" 1>&2
+    exit_usage_err
 fi
 
-# Source Directory
-if [ "${opt_clean}" != "true" ] && [ ! -d "${opt_srcdir}" ]
+if
+    [ "${opt_clean}" = "true" ]     &&
+    [ "${arg_clean}" != "src" ] &&
+    [ "${arg_clean}" != "rpm" ] &&
+    [ "${arg_clean}" != "deb" ] &&
+    [ "${arg_clean}" != "all" ]
 then
-    echo "Source directory does not exist: \"${opt_srcdir}\"" 1>&2
-    usage_err
+    echo "Invalid -${CHAR_OPT_CLEAN} argument: \"${arg_clean}\"" 1>&2
+    echo "Must be one of: {\"src\", \"rpm\", \"deb\", \"all\"}" 1>&2
+    exit_usage_err
 fi
 
-# File
-if [ -z "${opt_file}" ]
+if [ ! -d "${arg_srcdir}" ]
 then
-    echo "Invalid empty file option: \"${opt_file}\"" 1>&2
-    usage_err
+    echo "Source directory does not exist: \"${arg_srcdir}\"" 1>&2
+    exit_usage_err
 fi
 
-if [ "${opt_clean}" = "true" ]
+if [ ! -d "${arg_destdir}" ]
 then
-    if [ ! -e "${opt_file}" ]
-    then
-        echo "Cannot clean nonexistent file: \"${opt_file}\"" 1>&2
-        usage_err
-    fi
-
-    if [ -d "${opt_file}" ]
-    then
-        echo "Cannot clean directory: \"${opt_file}\"" 1>&2
-        usage_err
-    fi
-
-    if [ ! -f "${opt_file}" ]
-    then
-        echo "Cannot clean non-regular file: \"${opt_file}\"" 1>&2
-        usage_err
-    fi
-elif [ -e "${opt_file}" ]
-then
-    if [ -d "${opt_file}" ]
-    then
-        echo "Cannot overwrite directory: \"${opt_file}\"" 1>&2
-        usage_err
-    fi
-
-    if [ ! -f "${opt_file}" ]
-    then
-        echo "Cannot overwrite non-regular file: \"${opt_file}\"" 1>&2
-        usage_err
-    fi
-
-    if [ "${opt_force}" = "false" ]
-    then
-        echo "Must use -F to overwrite existing file: \"${opt_file}\"" 1>&2
-        usage_err
-    fi
-else
-    touch "${opt_file}"
-    if [ "${?}" -ne "0" ]
-    then
-        echo "Unable to create file: \"${opt_file}\"" 1>&2
-        usage_err
-    fi
-    rm -f "${opt_file}"
+    echo "Destination directory does not exist: \"${arg_destdir}\"" 1>&2
+    exit_usage_err
 fi
 
+# TODO: Validate arg_version.
+
+
+#
+# Build.
+#
+if [ "${opt_build}" = "true" ]
+then
+    echo "Building..."
+    exit 0
+    #tar -c -C "${opt_srcdir}" -f "${opt_file}" dboeger1-dotfiles-0.0.1
+fi
 
 #
 # Clean.
 #
 if [ "${opt_clean}" = "true" ]
 then
-    rm -f "${opt_file}"
-    if [ "${?}" -ne "0" ]
-    then
-        echo "Failed to delete file: \"${opt_file}\"" 1>&2
-        exit 1
-    fi
-
+    echo "Cleaning..."
     exit 0
-fi
-
-
-#
-# Build.
-#
-
-# TODO: Properly handle versioning, root directory, etc.
-tar -c -C "${opt_srcdir}" -f "${opt_file}" dboeger1-dotfiles-0.0.1
-if [ "${?}" -ne "0" ]
-then
-    echo "Failed to build archive: \"${opt_file}\"" 1>&2
-    exit 1
 fi
