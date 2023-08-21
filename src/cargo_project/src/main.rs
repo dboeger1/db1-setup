978f2894b62304b9ff06972ec1735ede6a65860a
David Michael Boeger <dmboeger@gmail.com>
2023-08-21 15:41:13 +0000

    Minor progress on cargo project.

--- src/cargo_project/src/main.rs
+++ src/cargo_project/src/main.rs
@@ -1,117 +1,147 @@
-use std::process::{Command, Stdio};
+#![allow(dead_code)]
 
+
+use std::{
+    path::PathBuf,
+    str::FromStr,
+};
+
+
+#[derive(Debug)]
+struct PathAB {
+    a: PathBuf,
+    b: PathBuf,
+}
 
 fn main() {
-    // Install dependencies. This should probably be broken up by domain.
-    install_dependencies();
+    let file_copies: [PathAB; 3] = [
+        PathAB {
+            a: PathBuf::from_str("tmux/.tmux.conf").unwrap(),
+            b: PathBuf::from_str("/root/tmux/.tmux.conf").unwrap(),
+        },
+        PathAB {
+            a: PathBuf::from_str("neovim/init.lua").unwrap(),
+            b: PathBuf::from_str("/root/.config/nvim/init.lua").unwrap(),
+        },
+        PathAB {
+            a: PathBuf::from_str("neovim/lazy-lock.json").unwrap(),
+            b: PathBuf::from_str("/root/.config/nvim/lazy-lock.json").unwrap(),
+        },
+    ];
 
-    // Install configuration files.
-    install_dotfiles();
+    for file_copy in file_copies {
+        println!("Copying File");
+
+        if file_copy.a.try_exists().unwrap() != true {
+            println!(
+                "\tSource does not exist: \"{}\"",
+                file_copy.a.to_string_lossy()
+            );
+            continue;
+        }
+
+        if file_copy.b.try_exists().unwrap() == true {
+            println!(
+                "\tDestination already exists: \"{}\"",
+                file_copy.b.to_string_lossy()
+            );
+            continue;
+        }
+
+        println!("    Source: \"{}\"", file_copy.a.to_string_lossy());
+        println!("    Destination: \"{}\"", file_copy.b.to_string_lossy());
+    }
 }
 
 
-fn install_dependencies() {
-    let dnf = Command::new("dnf")
-        .args([
-              "install",
-
-              // Utilities
-              "bash",
-              "coreutils",
-              "diffutils",
-              "fd-find",
-              "git",
-              "patch",
-              "ripgrep",
-              "tree",
-              "unzip",
-              "wget",
-
-              // Applications
-              "neovim",
-
-              // C
-              "cmake",
-              "gcc",
-              "make",
-
-              // C++
-              "gcc-c++",
-
-              // Python
-              "python",
-
-              // Javascript
-              "npm",
-
-              // RPM
-              "rpm-build",
-              "rpm-devel",
-              "rpmdevtools",
-              "rpmlint"
-        ])
-        .output()
-        .unwrap();
-    println!("{}", std::str::from_utf8(&dnf.stdout).unwrap());
-
-    install_rustup();
-}
-
-
-fn install_rustup() {
-    // Retrieve rustup installation script.
-    let curl = Command::new("curl")
-        .args([
-            // Enable/disable protocols.
-            "--proto", "=https",
-
-            // Use TLSv1.2 or greater.
-            "--tlsv1.2",
-
-            // Silent mode.
-            "-s",
-
-            // Show error even when -s is used.
-            "-S",
-
-            // Fail fast with no output on HTTP errors.
-            "-f",
-
-            // URL
-            "https://sh.rustup.rs"
-        ])
-        .stdout(Stdio::piped())
-        .spawn()
-        .unwrap();
-
-    // Install rustup.
-    let sh = Command::new("sh")
-        .args([
-              // Read commands from the standard input.
-              "-s",
-
-              // Denotes the end of the options and start of standard input.
-              "--",
-
-              // Disable confirmation prompt.
-              "-y"
-        ])
-        .stdin(Stdio::from(curl.stdout.unwrap()))
-        .output()
-        .unwrap();
-    println!("{}", std::str::from_utf8(&sh.stdout).unwrap());
-}
+//fn install_dependencies() {
+//    let dnf = Command::new("dnf")
+//        .args([
+//              "install",
+//
+//              // Utilities
+//              "bash",
+//              "coreutils",
+//              "diffutils",
+//              "fd-find",
+//              "git",
+//              "patch",
+//              "ripgrep",
+//              "tree",
+//              "unzip",
+//              "wget",
+//
+//              // Applications
+//              "neovim",
+//
+//              // C
+//              "cmake",
+//              "gcc",
+//              "make",
+//
+//              // C++
+//              "gcc-c++",
+//
+//              // Python
+//              "python",
+//
+//              // Javascript
+//              "npm",
+//
+//              // RPM
+//              "rpm-build",
+//              "rpm-devel",
+//              "rpmdevtools",
+//              "rpmlint"
+//        ])
+//        .output()
+//        .unwrap();
+//    println!("{}", std::str::from_utf8(&dnf.stdout).unwrap());
+//
+//    install_rustup();
+//}
 
 
-fn install_dotfiles() {
-    install_tmux_config();
-    install_neovim_config();
-}
-
-
-fn install_tmux_config() {
-}
-
-
-fn install_neovim_config() {
-}
+//fn install_rustup() {
+//    // Retrieve rustup installation script.
+//    let curl = Command::new("curl")
+//        .args([
+//            // Enable/disable protocols.
+//            "--proto", "=https",
+//
+//            // Use TLSv1.2 or greater.
+//            "--tlsv1.2",
+//
+//            // Silent mode.
+//            "-s",
+//
+//            // Show error even when -s is used.
+//            "-S",
+//
+//            // Fail fast with no output on HTTP errors.
+//            "-f",
+//
+//            // URL
+//            "https://sh.rustup.rs"
+//        ])
+//        .stdout(Stdio::piped())
+//        .spawn()
+//        .unwrap();
+//
+//    // Install rustup.
+//    let sh = Command::new("sh")
+//        .args([
+//              // Read commands from the standard input.
+//              "-s",
+//
+//              // Denotes the end of the options and start of standard input.
+//              "--",
+//
+//              // Disable confirmation prompt.
+//              "-y"
+//        ])
+//        .stdin(Stdio::from(curl.stdout.unwrap()))
+//        .output()
+//        .unwrap();
+//    println!("{}", std::str::from_utf8(&sh.stdout).unwrap());
+//}
