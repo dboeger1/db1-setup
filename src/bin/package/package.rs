1941f9c39a4342ce9d6b0d760e4c164abaea542c
David Michael Boeger <dmboeger@gmail.com>
2024-01-25 19:57:35 +0000

    Deduplicated tar code.

--- src/bin/package/package.rs
+++ src/bin/package/package.rs
@@ -1,12 +1,24 @@
 use crate::{
     error::Error,
     platform::PLATFORM,
+    values::DIR_PROJECT_PACKAGES,
 };
 
 
 pub(crate) fn package() -> Result<(), Error> {
-    (PLATFORM
-        .as_ref()
-        .unwrap()
-        .archive_sources)()
+    if DIR_PROJECT_PACKAGES.exists() {
+        return Err(Error {
+            message: format!(
+                "cannot overwrite existing directory: \"{}\"",
+                DIR_PROJECT_PACKAGES.to_string_lossy(),
+            ),
+            source: None,
+        });
+    }
+
+    let platform_data = PLATFORM.as_ref().unwrap();
+    (platform_data.archive_sources)()?;
+    //(platform_data.package)()?;
+
+    Ok(())
 }
