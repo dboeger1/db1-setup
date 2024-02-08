65a3138c1d9ad80b7fe1cf3347e81f37c27f26c1
David Michael Boeger <dmboeger@gmail.com>
2024-02-08 09:13:44 +0000

    I'm tired.

--- db1-setup/tests/main.rs
+++ db1-setup/tests/main.rs
@@ -1,4 +1,10 @@
 #[test]
 fn test_args() {
-    let args = db1_setup::subcommand::c::Args{};
+    let _args = db1_setup::Args {
+        subcommand: db1_setup::Subcommand::Cpp(
+            db1_setup::cpp::Args {
+                subcommand: db1_setup::cpp::Subcommand::Install,
+            },
+        ),
+    };
 }
